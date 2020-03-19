4f398e6f09f435e5a1304684d7a186d889395035
tgrisham <tom.grisham@kellogg.northwestern.edu>
2020-03-19 22:34:43 +0000

    Final First Draft

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
-require "sinatra"                                                                     #
+require "sinatra"
+require "sinatra/cookies"                                                             #
 require "sinatra/reloader" if development?                                            #
 require "sequel"                                                                      #
 require "logger"                                                                      #
-require "twilio-ruby"                                                                 #
 require "bcrypt"                                                                      #
+require "twilio-ruby"
+require "geocoder"                                                                #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -14,5 +16,158 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
+hoods_table = DB.from(:hoods)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+# homepage and list of neighborhoods (aka "index")
+get "/" do
+    puts "params: #{params}"
+
+    @hoods = hoods_table.all.to_a
+    pp @hoods
+
+    view "hoods"
+end
+
+# neighborhood details (aka "show")
+get "/hoods/:id" do
+    puts "params: #{params}"
+
+    @users_table = users_table
+    @hood = hoods_table.where(id: params[:id]).to_a[0]
+    pp @hood
+
+    @rsvps = rsvps_table.where(hood_id: @hood[:id]).to_a
+    @going_count = rsvps_table.where(hood_id: @hood[:id], going: true).count
+
+    view "hood"
+end
+
+# display the rsvp form (aka "new")
+get "/hoods/:id/rsvps/new" do
+    puts "params: #{params}"
+
+    @hood = hoods_table.where(id: params[:id]).to_a[0]
+    view "new_rsvp"
+end
+
+# receive the submitted rsvp form (aka "create")
+post "/hoods/:id/rsvps/create" do
+    puts "params: #{params}"
+
+    # first find the neighborhood that rsvp'ing for
+    @hood = hoods_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    rsvps_table.insert(
+        hood_id: @hood[:id],
+        user_id: session["user_id"],
+        comments: params["comments"],
+        going: params["going"]
+    )
+
+    redirect "/hoods/#{@hood[:id]}"
+end
+
+# display the rsvp form (aka "edit")
+get "/rsvps/:id/edit" do
+    puts "params: #{params}"
+
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @hood = hoods_table.where(id: @rsvp[:hood_id]).to_a[0]
+    view "edit_rsvp"
+end
+
+# receive the submitted rsvp form (aka "update")
+post "/rsvps/:id/update" do
+    puts "params: #{params}"
+
+    # find the rsvp to update
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    # find the rsvp's hood
+    @hood = hoods_table.where(id: @rsvp[:hood_id]).to_a[0]
+
+    if @current_user && @current_user[:id] == @rsvp[:id]
+        rsvps_table.where(id: params["id"]).update(
+            going: params["going"],
+            comments: params["comments"]
+        )
+
+        redirect "/hoods/#{@hood[:id]}"
+    else
+        view "error"
+    end
+end
+
+# delete the rsvp (aka "destroy")
+get "/rsvps/:id/destroy" do
+    puts "params: #{params}"
+
+    rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @hood = hoods_table.where(id: rsvp[:hood_id]).to_a[0]
+
+    rsvps_table.where(id: params["id"]).delete
+
+    redirect "/hoods/#{@hood[:id]}"
+end
+
+# display the signup form (aka "new")
+get "/users/new" do
+    view "new_user"
+end
+
+# receive the submitted signup form (aka "create")
+post "/users/create" do
+    puts "params: #{params}"
+
+    # if there's already a user with this email, skip!
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+        users_table.insert(
+            name: params["name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+        )
+
+        redirect "/logins/new"
+    end
+end
+
+# display the login form (aka "new")
+get "/logins/new" do
+    view "new_login"
+end
+
+# receive the submitted login form (aka "create")
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            redirect "/"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+# logout user
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
+end
--- createdb.rb
+++ createdb.rb
@@ -1,35 +1,59 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
 require "sequel"                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
-DB = Sequel.connect(connection_string)                                                #
+DB ||= Sequel.connect(connection_string)                                              #
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+
+# New domain model - adds users
+DB.create_table! :hoods do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
+  String :walkability
+  String :nightlife
   String :location
 end
 DB.create_table! :rsvps do
   primary_key :id
-  foreign_key :event_id
+  foreign_key :hood_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+hoods_table = DB.from(:hoods)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+hoods_table.insert(title: "Downtown / Waterfront", 
+                    description: "If you’re looking for your own slice of New York City on the West Coast, downtown Seattle is a great place to start. Everything you could ever want from a big city is packed into Seattle’s very geographically manageable downtown. Catch a concert at Benaroya Hall, or enjoy a book in the Louvre Pyramid.",
+                    walkability: "8/10",
+                    nightlife: "8/10",
+                    location: "downtown, seattle")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+hoods_table.insert(title: "Capitol Hill / Seattle Metro", 
+                    description: "Capitol Hill, located east of and just up the hill from downtown, acts as a sort of amphitheater stage to the residential neighborhoods surrounding it — this is where a lot of Seattle comes for entertainment. If you like nightlife, theaters, music, and living loud, Capitol Hill is your stop.",
+                    walkability: "9/10",
+                    nightlife: "10/10",
+                    location: "capitol hill, seattle")
+
+hoods_table.insert(title: "Queen Anne / Cascade", 
+                    description: "Queen Anne is on its own little peninsula, jutting northwest from downtown and South Lake Union. It’s mostly upscale and pretty posh, but don’t take that the wrong way — this area is also a sweet place to hang out. If you’re the book-with-your-coffee and cocktails-with-a-few-friends type, this is your neighborhood.",
+                    walkability: "7/10",
+                    nightlife: "7/10",
+                    location: "queen anne, seattle")
+
+hoods_table.insert(title: "Ballard", 
+                    description: "On the north shore opposite Queen Anne, you’ll find Ballard. It was once a separate city of Scandinavian immigrants, centered around fishing. These days, it’s a good example of a historic area that honors its heritage while moving forward into a new era. Ballard is an incredible mix of old, European-style brick buildings and new-construction condos.",
+                    walkability: "8/10",
+                    nightlife: "8/10",
+                    location: "ballard, seattle")
+                    
+puts "Success!"
